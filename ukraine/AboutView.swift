31a2aa216888a32403524a1e175d5ae39e247a14
Victor Gustafsson <victor.gustafsson@knowit.se>
2023-05-15 18:07:44 +0200

    adds back GoogleService-info file so teacher can review code

--- ukraine/AboutView.swift
+++ ukraine/AboutView.swift
@@ -10,10 +10,9 @@ import SwiftUI
 struct AboutView: View {
     
 // TODO: FIX
-    let textOne: String = """
+    let text: String = """
  For Ukraine is authored by Victor Gustafsson as a final project at IT-Högskolan in Gothenburg. The app wouldn't been possible without help from Yuliia Kyzyk.
-"""
-    let textTwo = """
+ 
  As a Swedish citizen, we often take for granted all the instituions a refugee, immigrant or asylum seeker has to go through. This was something that my girlfriend Yuliia shined a light on, and when I got the opportunity to develop an app as a final project before my graduation I knew what to do.
  The app gathers all the most useful links that can be helpful in one place.
  """
@@ -22,7 +21,7 @@ struct AboutView: View {
             Group {
                 Text("For Ukraine").font(.title).bold()
                 Group {
-                    Text(textOne + textTwo)
+                    Text(text)
 
                 }.padding()
             }.frame(maxWidth: .infinity, alignment: .center).foregroundColor(.white)
